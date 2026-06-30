42d642e5019fe39b4aa29af065b2219f034d3e45
msf <faisalsohaib1997@gmail.com>
2026-06-30 17:20:04 +0200

    fix batch_run_linux

--- batch_run_linux.sh
+++ batch_run_linux.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-GPU_ID=2
-EPOCHS=100
-
-for LAYERS in 3 4 5
-do
-    for NODES in 1 2 3
-    do
-        CUDA_VISIBLE_DEVICES=$GPU_ID python main.py \
+GPU_ID=0
+EPOCHS=150
+LAYERS=5
+NODES=2
+CUDA_VISIBLE_DEVICES=$GPU_ID python main.py \
             --layers $LAYERS \
             --nodes $NODES \
             --epochs $EPOCHS
-    done
-done
+
